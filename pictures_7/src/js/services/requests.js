dd47cf64feaaae3c707c89fd29ee48b832a53e07
Igor <example@gmail.com>
2024-07-07 15:54:55 +0200

    loading from the database dynamically

---
+++ pictures_7/src/js/services/requests.js
@@ -0,0 +1,22 @@
+//Асинхронная функция для отправки данных:
+//postData: функция, которая отправляет данные формы на сервер и возвращает ответ сервера
+const postData = async (url, data) => {
+	let res = await fetch(url, {
+		method: 'POST',
+		body: data
+	});
+
+	return await res.text();
+};
+
+const getResource = async (url) => {
+	let res = await fetch(url);
+
+	if (!res.ok) {
+		throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+	}
+
+	return await res.json();
+};
+
+export { postData, getResource };
